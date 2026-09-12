7c0c80619e79837387d787f684655a4402c16adb
Thiago Figueiroa Oliveira <thiagofigueiroaoliveira@gmail.com>
2026-09-12 13:39:20 -0300

    feat(plugins): replace diffs.nvim with codediff.nvim and enable lua formatting on save

---
+++ lua/plugins/codediff.lua
@@ -0,0 +1,3 @@
+vim.pack.add {
+  Gh "esmuellert/codediff.nvim",
+}
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,7 +11,7 @@ Later(function()
     format_on_save = function(bufnr)
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
-        -- lua = true,
+        lua = true,
         python = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
